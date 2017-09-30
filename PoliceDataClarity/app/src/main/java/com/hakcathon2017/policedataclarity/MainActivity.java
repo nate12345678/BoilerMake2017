213bea4dcf0dbe027724e8e868d00fd49bf2f3bf
Nate Post <nate12345678@users.noreply.github.com>
2017-09-30 03:02:12 -0400

    Psuedo-functional GUI

--- PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/MainActivity.java
+++ PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/MainActivity.java
@@ -12,14 +12,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
+
+	LinearLayout insightsView;
+	LinearLayout rankingView;
+	LinearLayout weekView;
+	LinearLayout careerView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		insightsView = (LinearLayout) findViewById(R.id.insightsView);
+		rankingView = (LinearLayout) findViewById(R.id.rankingView);
+		weekView = (LinearLayout) findViewById(R.id.weekView);
+		careerView = (LinearLayout) findViewById(R.id.careerView);
+
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
@@ -80,18 +92,21 @@ public class MainActivity extends AppCompatActivity
 		// Handle navigation view item clicks here.
 		int id = item.getItemId();
 
-		if (id == R.id.nav_camera) {
-			// Handle the camera action
-		} else if (id == R.id.nav_gallery) {
+		insightsView.setVisibility(View.GONE);
+		rankingView.setVisibility(View.GONE);
+		weekView.setVisibility(View.GONE);
+		careerView.setVisibility(View.GONE);
 
-		} else if (id == R.id.nav_slideshow) {
-
-		} else if (id == R.id.nav_manage) {
-
-		} else if (id == R.id.nav_share) {
-
-		} else if (id == R.id.nav_send) {
-
+		if (id == R.id.nav_insights) {
+			insightsView.setVisibility(View.VISIBLE);
+		} else if (id == R.id.nav_ranking) {
+			rankingView.setVisibility(View.VISIBLE);
+		} else if (id == R.id.nav_week) {
+			weekView.setVisibility(View.VISIBLE);
+		} else if (id == R.id.nav_career) {
+			careerView.setVisibility(View.VISIBLE);
+		} else if (id == R.id.nav_settings) {
+			insightsView.setVisibility(View.VISIBLE);
 		}
 
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
