9a785ef7bee45dd6e64f6ba80922407cbe2ef2c7
Nate Post <nate12345678nate@gmail.com>
2017-09-30 13:58:28 -0500

    Adding URLs in Globals

--- PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/Globals.java
+++ PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/Globals.java
@@ -8,4 +8,9 @@ package com.hakcathon2017.policedataclarity;
 
 public class Globals {
 	static String username = "NULL";
+	final String allSched = "%20and%20Type%20eq%20%27SCHED%27&$orderby=StartTime%20asc";
+	final String amountDaysSched = "%20and%20Type%20eq%20%27SCHED%27%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String allDisp = "%20and%20Type%20eq%20%27DSP%27&$orderby=StartTime%20asc";
+	final String amountDaysDisp = "%20and%20Type%20eq%20%27DSP%27%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String allStkDisp = "%20and%20Type%20eq%20%27STKDSP%27&$orderby=StartTime%20asc";
 }
