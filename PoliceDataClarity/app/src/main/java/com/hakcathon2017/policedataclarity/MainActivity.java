929a5c661a88bb4b268b623b411c2a5ec828b561
Nate Post <nate12345678@users.noreply.github.com>
2017-09-29 22:58:37 -0400

    Made Project

---
+++ PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hakcathon2017.policedataclarity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
