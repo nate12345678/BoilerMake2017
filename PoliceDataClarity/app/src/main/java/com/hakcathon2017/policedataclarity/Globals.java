a9f4f5dfd98abc9eb50088825a5a07037da37a21
Nate Post <nate12345678nate@gmail.com>
2017-09-30 14:51:56 -0500

    Added Global URLs

---
+++ PoliceDataClarity/app/src/main/java/com/hakcathon2017/policedataclarity/Globals.java
@@ -0,0 +1,24 @@
+package com.hakcathon2017.policedataclarity;
+
+/**
+ * Copyright (C) 2017 Nathan Post. All rights reserved.
+ * Created by Nathan Post on 9/30/17.
+ * This class belongs to the PoliceDataClarity project.
+ */
+
+public class Globals {
+	static String username = "NULL";
+	final String allStat = "$orderby=StartTime%20asc";
+	final String allSched = "%20and%20Type%20eq%20%27SCHED%27&$orderby=StartTime%20asc";
+	final String amountDaysSched = "%20and%20Type%20eq%20%27SCHED%27%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String lastWeekSched = "%20and%20Type%20eq%20%27SCHED%27%20and%20month(StartTime)%20eq%208%20and%20day(StartTime)%20sub%207%20eq%2022&$orderby=StartTime%20asc";
+	final String allDisp = "%20and%20Type%20eq%20%27DSP%27&$orderby=StartTime%20asc";
+	final String amountDaysDisp = "%20and%20Type%20eq%20%27DSP%27%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String lastWeekDisp = "%20and%20Type%20eq%20%27DSP%27%20and%20month(StartTime)%20eq%208%20and%20day(StartTime)%20sub%207%20eq%2022&$orderby=StartTime%20asc";
+	final String allStkDisp = "%20and%20Type%20eq%20%27STKDSP%27&$orderby=StartTime%20asc";
+	final String amountDaysStkDisp = "%20and%20Type%20eq%20%27STKDSP%27%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String lastWeekStkDisp = "%20and%20Type%20eq%20%27STKDSP%27%20and%20month(StartTime)%20eq%208%20and%20day(StartTime)%20sub%207%20eq%2022&$orderby=StartTime%20asc";
+	final String allStops = "%20and%20endswith(Type,%20%27STOP%27)&$orderby=StartTime%20asc";
+	final String amountDaysStops = "%20and%20endswith(Type,%20%27STOP%27)%20and%20month(StartTime)%20eq%206%20and%20day(StartTime)%20ge%201%20and%20day(StartTime)%20le%207&$orderby=StartTime%20asc";
+	final String lastWeekStops = "%20and%20endswith(Type,%20%27STOP%27)%20and%20month(StartTime)%20eq%208%20and%20day(StartTime)%20sub%207%20eq%2022&$orderby=StartTime%20asc";
+}
